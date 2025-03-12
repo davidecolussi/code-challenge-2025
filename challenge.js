7b405e783e067a8743270a1eb388cd9206f2e451
fk0217 <federico.baron@consulenti.allitude.it>
2025-03-12 20:36:13 +0100

    basta

--- challenge.js
+++ challenge.js
@@ -1,10 +1,10 @@
-import { appendFileSync, createReadStream } from "fs";
+import { appendFileSync, createReadStream, writeFileSync } from "fs";
 import * as path from "path";
 import { createInterface } from "readline";
 
 const inputBasePath = "./input";
 const outputBasePath = "./output";
-const inputFile = "0-demo.txt";
+const inputFile = "7-mckibben.txt";
 
 const line_counter = (
   (i = 0) =>
@@ -19,7 +19,7 @@ const lineReader = createInterface({
 let budget,
   numResources,
   numTurns,
-  availResuorces = {},
+  resources = {},
   turns = [];
 
 lineReader.on("line", (line, index = line_counter()) => {
@@ -31,7 +31,7 @@ lineReader.on("line", (line, index = line_counter()) => {
     numResources = Number(values[1]);
     numTurns = Number(values[2]);
   } else if (index <= numResources + 1) {
-    availResuorces[values[0]] = {
+    resources[values[0]] = {
       id: values[0],
       activationCost: Number(values[1]),
       periodicCost: Number(values[2]),
@@ -41,6 +41,12 @@ lineReader.on("line", (line, index = line_counter()) => {
       numPowered: Number(values[6]),
       specialEffects: values[7],
       specialEffectsValue: values[8] && Number(values[8]), // optional
+      status: {
+        active: false,
+        remainingTurns: Number(values[5]),
+        remainingActiveTurns: Number(values[3]),
+        remainingRecoverTurns: undefined,
+      },
     };
   } else {
     turns.push({
@@ -53,8 +59,110 @@ lineReader.on("line", (line, index = line_counter()) => {
 
 lineReader.on("close", async () => {
   console.log("reader closed");
-  console.log(budget, numResources, numTurns);
-  console.log(availResuorces, turns);
 
-  appendFileSync(path.join(outputBasePath, inputFile), `Riga di prova \n`);
+  writeFileSync(path.join(outputBasePath, inputFile), "");
+  turns.forEach(({ min, max, profit }, i) => {
+    Object.values(resources)
+      // aggiornare i contatori attive
+      .forEach((r) => {
+        if (r.status.active) {
+          r.status.remainingTurns--;
+          r.status.remainingActiveTurns--;
+          if (r.status.remainingTurns === 0) {
+            r.status.active = false;
+          } else if (r.status.remainingActiveTurns === 0) {
+            r.status.active = false;
+            r.status.recoverTurns = r.recoverTurns;
+          }
+        } else {
+          // aggiornare i contatori non attive
+          if (r.status.recoverTurns !== undefined) {
+            r.status.recoverTurns--;
+          }
+        }
+      });
+
+    const selectedArray = [];
+    const maxNumPerTurn = 2;
+
+    // decisione su quali attivare
+    const inBuget = getResourcesInBudget(min, max, profit).sort(
+      (a, b) => b[1] - a[1]
+    );
+    if (!inBuget.length || inBuget[0][1] < 0) {
+      return;
+    }
+    // attiva la prima senza poteri
+    const selected = resources[inBuget[0][0]];
+    selectedArray.push(selected);
+    resources[selected.id].status.active = true;
+
+    for (let i = 1; i < maxNumPerTurn; i++) {
+      const inBuget2 = getResourcesInBudget(min, max, profit).sort(
+        (a, b) => b[1] - a[1]
+      );
+      if (inBuget2.length && inBuget2[0][1] >= 0) {
+        const selected2 = resources[inBuget2[0][0]];
+        selectedArray.push(selected2);
+        resources[selected2.id].status.active = true;
+      }
+    }
+
+    console.log(selectedArray);
+
+    appendFileSync(
+      path.join(outputBasePath, inputFile),
+      `${i} ${selectedArray.length} ${selectedArray
+        .map((r) => r.id)
+        .join(" ")}\n`
+    );
+
+    // aggiorna budget
+    const currProfit = getProfit([], min, max, profit);
+    const periodicCost = getPeriodicCost([]);
+    budget -= selectedArray.reduce((count, r) => count + r.activationCost, 0);
+    budget -= periodicCost;
+    budget += currProfit;
+    console.log(budget);
+  });
 });
+
+function getProfit(additionalResources, tm, tx, tr) {
+  const n = Object.values(resources)
+    .filter((r) => r.status.active)
+    .concat(additionalResources)
+    .reduce((count, r) => count + r.numPowered, 0);
+  return n >= tm ? Math.min(n, tx) * tr : 0;
+}
+
+function getPeriodicCost(additionalResources) {
+  return Object.values(resources)
+    .filter((r) => r.status.active)
+    .concat(additionalResources)
+    .reduce((count, r) => count + r.periodicCost, 0);
+}
+
+function getResourcesInBudget(tm, tx, tr) {
+  return (
+    Object.values(resources)
+      .filter(
+        (r) =>
+          !r.status.active &&
+          (r.status.recoverTurns === undefined ||
+            r.status.recoverTurns === 0) &&
+          r.status.remainingTurns > 0
+      )
+      // .filter((r) => r.numPowered > 30 || r.specialEffectsValue > 0)
+      // .filter((r) => r.specialEffects === "X" || r.specialEffectsValue > 0)
+      .map((r) => {
+        if (budget < r.activationCost) {
+          return [r.id, -1];
+        }
+        const profit = getProfit([r], tm, tx, tr);
+        return [
+          r.id,
+          budget + profit - r.activationCost - getPeriodicCost([r]),
+        ];
+      })
+  );
+}
